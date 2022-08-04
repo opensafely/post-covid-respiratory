b60d1e16f44f10b86eb00a23b1addd942ec84f2a
Venexia Walker <venexia.walker@bristol.ac.uk>
2022-08-04 13:58:46 +0100

    Make active analyses resemble outcome tracker

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -6,36 +6,110 @@ fs::dir_create(here::here("lib"))
 
 df <- data.frame(name = character(), # Flexibility for tmp files to be run
                  cohort = character(),
-                 exposure = character(),
-                 outcome = character(),
-                 strata = character(), # Input for cox-ipw; variables to be used as strata
-                 covariate_other = character(), # Input for cox-ipw; covariates other than age and sex
-                 covariate_protect = character(), # Input for cox-ipw; stop model if these covariates are removed
-                 ipw = logical(), # Input for cox-ipw; TRUE to perform IPW
-                 cutpoints = character(),
-                 subgroup_covid_history = logical(),
-                 subgroup_covid_hospitalised = logical(),
-                 subgroup_covid_nonhospitalised = logical(),
-                 subgroup_agegroups = character(),
-                 subgroup_other = character(), # Semi-colon seperated list of variables to perform subgroups for
+                 exposure = character(), # Input for cox-ipw
+                 outcome = character(), # Input for cox-ipw
+                 strata = character(), # Input for cox-ipw
+                 covariate_sex = character(), # Input for cox-ipw 
+                 covariate_age = character(), # Input for cox-ipw
+                 covariate_other = character(), # Input for cox-ipw
+                 ipw = logical(), # Input for cox-ipw
+                 cutpoints = character(), # Input for cox-ipw
+                 main = logical(),
+                 sub_covid_hospitalised = logical(),
+                 sub_covid_nonhospitalised = logical(),
+                 sub_covid_history = logical(),
+                 sub_sex_female = logical(),
+                 sub_sex_male = logical(),
+                 sub_age_18_39 = logical(),
+                 sub_age_40_59 = logical(),
+                 sub_age_60_79 = logical(),
+                 sub_age_80_110 = logical(),
+                 sub_ethnicity_white = logical(),
+                 sub_ethnicity_black = logical(),
+                 sub_ethnicity_mixed = logical(),
+                 sub_ethnicity_asian = logical(),
+                 sub_ethnicity_other = logical(),
+                 priorhistory_var = character(),
+                 sub_priorhistory_true = logical(),
+                 sub_priorhistory_false = logical(),
+                 sub_smokingstatus_ever = logical(),
+                 sub_smokingstatus_never = logical(),
+                 sub_smokingstatus_current = logical(),
                  stringsAsFactors = FALSE)
 
-# Add outcome 'acute myocardial infarction' ------------------------------------
+# For example, add outcome AMI with all subgroups ------------------------------
 
-df[nrow(df)+1,] <- c(name = "VAX_AMI_2TP",
+df[nrow(df)+1,] <- c(name = "VAX_AMI",
                      cohort = "vaccinated",
                      exposure = "exp_date_covid19_confirmed",
                      outcome = "out_date_ami",
                      strata = "cov_cat_region",
-                     covariate_other = "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_num_age;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_cat_smoking_status;cov_bin_carehome_status;cov_bin_lipid_medications;cov_bin_antiplatelet_medications;cov_bin_anticoagulation_medications;cov_bin_combined_oral_contraceptive_pill;cov_bin_hormone_replacement_therapy;cov_bin_ami;cov_bin_all_stroke;cov_bin_other_arterial_embolism;cov_bin_vte;cov_bin_hf;cov_bin_angina;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_depression;cov_bin_chronic_obstructive_pulmonary_disease;cov_cat_sex",
-                     covariate_protect = "cov_cat_ethnicity;cov_cat_region;cov_cat_sex;cov_num_age",
+                     covariate_sex = "cov_cat_sex",
+                     covariate_age = "cov_num_age",
+                     covariate_other = "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_smoking_status;cov_bin_carehome_status;cov_bin_lipid_medications;cov_bin_antiplatelet_medications;cov_bin_anticoagulation_medications;cov_bin_combined_oral_contraceptive_pill;cov_bin_hormone_replacement_therapy;cov_bin_ami;cov_bin_all_stroke;cov_bin_other_arterial_embolism;cov_bin_vte;cov_bin_hf;cov_bin_angina;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_depression;cov_bin_chronic_obstructive_pulmonary_disease",
                      ipw = TRUE,
                      cutpoints = "28;197",
-                     subgroup_covid_history = TRUE,
-                     subgroup_covid_hospitalised = TRUE,
-                     subgroup_covid_nonhospitalised = TRUE,
-                     subgroups = "sub_cat_agegroup;cov_cat_ethnicity;cov_cat_sex;sub_bin_ate")
+                     main = TRUE,
+                     sub_covid_hospitalised = TRUE,
+                     sub_covid_nonhospitalised = TRUE,
+                     sub_covid_history = TRUE,
+                     sub_sex_female = TRUE,
+                     sub_sex_male = TRUE,
+                     sub_age_18_39 = TRUE,
+                     sub_age_40_59 = TRUE,
+                     sub_age_60_79 = TRUE,
+                     sub_age_80_110 = TRUE,
+                     sub_ethnicity_white = TRUE,
+                     sub_ethnicity_black = TRUE,
+                     sub_ethnicity_mixed = TRUE,
+                     sub_ethnicity_asian = TRUE,
+                     sub_ethnicity_other = TRUE,
+                     priorhistory_var = "sub_bin_ate",
+                     sub_priorhistory_true = TRUE,
+                     sub_priorhistory_false = TRUE,
+                     sub_smokingstatus_ever = TRUE,
+                     sub_smokingstatus_never = TRUE,
+                     sub_smokingstatus_current = TRUE)
 
-# Save active analyses list ----------------------------------------------------
+# For example, test outcome AMI using prior history of AMI rather than ATE -----
 
-saveRDS(df, file = "lib/active_analyses.rds")
+df[nrow(df)+1,] <- c(name = "VAX_AMI_AMIpriorhistory",
+                     cohort = "vaccinated",
+                     exposure = "exp_date_covid19_confirmed",
+                     outcome = "out_date_ami",
+                     strata = "cov_cat_region",
+                     covariate_sex = "cov_cat_sex",
+                     covariate_age = "cov_num_age",
+                     covariate_other = "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_smoking_status;cov_bin_carehome_status;cov_bin_lipid_medications;cov_bin_antiplatelet_medications;cov_bin_anticoagulation_medications;cov_bin_combined_oral_contraceptive_pill;cov_bin_hormone_replacement_therapy;cov_bin_ami;cov_bin_all_stroke;cov_bin_other_arterial_embolism;cov_bin_vte;cov_bin_hf;cov_bin_angina;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_depression;cov_bin_chronic_obstructive_pulmonary_disease",
+                     ipw = TRUE,
+                     cutpoints = "28;197",
+                     main = FALSE,
+                     sub_covid_hospitalised = FALSE,
+                     sub_covid_nonhospitalised = FALSE,
+                     sub_covid_history = FALSE,
+                     sub_sex_female = FALSE,
+                     sub_sex_male = FALSE,
+                     sub_age_18_39 = FALSE,
+                     sub_age_40_59 = FALSE,
+                     sub_age_60_79 = FALSE,
+                     sub_age_80_110 = FALSE,
+                     sub_ethnicity_white = FALSE,
+                     sub_ethnicity_black = FALSE,
+                     sub_ethnicity_mixed = FALSE,
+                     sub_ethnicity_asian = FALSE,
+                     sub_ethnicity_other = FALSE,
+                     priorhistory_var = "sub_bin_ami",
+                     sub_priorhistory_true = TRUE,
+                     sub_priorhistory_false = TRUE,
+                     sub_smokingstatus_ever = FALSE,
+                     sub_smokingstatus_never = FALSE,
+                     sub_smokingstatus_current = FALSE)
+
+
+# Check names are unique and save active analyses list -------------------------
+
+if (length(unique(df$name))==nrow(df)) {
+  saveRDS(df, file = "lib/active_analyses.rds")
+} else {
+  stop(paste0("ERROR: names must be unique in active analyses table"))
+}
--- analysis/model/make_model_input.R
+++ analysis/model/make_model_input.R
@@ -12,70 +12,292 @@ fs::dir_create(here::here("output", "model_input"))
 args <- commandArgs(trailingOnly=TRUE)
 
 if(length(args)==0){
-  
+  name <- "VAX_ATE_2TP"
 }else{
-  arg <- args[[1]]
+  name <- args[[1]]
 }
 
 # Load active analyses ---------------------------------------------------------
 
 active_analyses <- readr::read_rds("lib/active_analyses.rds")
-i = 1
-
+active_analyses <- active_analyses[active_analyses$name==name,]
+  
 # Load data --------------------------------------------------------------------
 
-input <- readr::read_rds(paste0("../post-covid-vaccinated/output/input_",active_analyses$cohort[i],"_stage1.rds")) # For testing purposes
+input <- readr::read_rds(paste0("output/input_",active_analyses$cohort,"_stage1.rds"))
 
 # Restrict to required variables -----------------------------------------------
 
 input <- input[,unique(c("patient_id",
                          "index_date",
                          "death_date",
-                         active_analyses$exposure[i], 
-                         active_analyses$outcome[i],
-                         unlist(strsplit(active_analyses$strata[i], split = ";")),
-                         unlist(strsplit(active_analyses$covariate_other[i], split = ";")),
+                         active_analyses$exposure, 
+                         active_analyses$outcome,
+                         unlist(strsplit(active_analyses$strata, split = ";")),
+                         unlist(strsplit(active_analyses$covariate_other, split = ";")),
                          "sub_cat_covid19_hospital",
                          "sub_bin_covid19_confirmed_history",
-                         unlist(strsplit(active_analyses$subgroup_other[i], split = ";"))))]
+                         "cov_cat_sex",
+                         "cov_num_age",
+                         "cov_cat_ethnicity",
+                         active_analyses$priorhistory_var,
+                         "cov_cat_smoking_status"))]
 
-# Make main analysis input -----------------------------------------------------
+# Make model input: main -------------------------------------------------------
 
 df <- input[input$sub_bin_covid19_confirmed_history==FALSE,]
-saveRDS(df, file = paste0("output/model_input/",active_analyses$name[i],"-main.rds"))
+
+saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-main.rds"))
+print(paste0("Saved: output/model_input/",active_analyses$name,"-main.rds"))
 rm(df)
 
-# Make covid history subgroup --------------------------------------------------
+# Make model input: sub_covid_hospitalised -------------------------------------
 
-if (active_analyses$subgroup_covid_history[i]==TRUE) {
+## TO BE COMPLETED ONCE CESORING OF HOSPITALISED CASES AGREED
+
+# Make model input: sub_covid_nonhospitalised ----------------------------------
+
+## TO BE COMPLETED ONCE CESORING OF HOSPITALISED CASES AGREED
+
+# Make model input: sub_covid_history ------------------------------------------
+
+if (active_analyses$sub_covid_history==TRUE) {
+  
   df <- input[input$sub_bin_covid19_confirmed_history==TRUE,]
-  saveRDS(df, file = paste0("output/model_input/",active_analyses$name[i],"-covid_history.rds"))
-  print(paste0("Saved: output/model_input/",active_analyses$name[i],"-covid_history.rds"))
+  
+  df[,c("sub_bin_covid19_confirmed_history")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_covid_history.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_covid_history.rds"))
   rm(df)
-}
-
-# Make covid hospitalised subgroups --------------------------------------------
-
-## To be completed - requires special censoring criteria
-
-# Make covid non-hospitalised subgroups ----------------------------------------
-
-## To be completed - requires special censoring criteria
-
-# Make all other subgroups -----------------------------------------------------
-
-subgroups <- unlist(strsplit(active_analyses$subgroup_other, split = ";"))
-
-for (x in subgroups) {
-  values <- unique(as.character(input[,x]))
-  for (y in values) {
-    df <- input[input$sub_bin_covid19_confirmed_history==FALSE,]
-    df <- dplyr::rename(df, "subgroup" = tidyselect::all_of(x))
-    df <- df[df$subgroup==y,]
-    y <- tolower(gsub(" ","_",y))
-    saveRDS(df, file = paste0("output/model_input/",active_analyses$name[i],"-",x,"-",y,".rds"))
-    print(paste0("Saved: output/model_input/",active_analyses$name[i],"-",x,"-",y,".rds"))
-    rm(df)
-  }
   
 }
+
+# Make model input: sub_sex_female ---------------------------------------------
+
+if (active_analyses$sub_sex_female==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_sex=="Female",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_sex")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_sex_female.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_sex_female.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_sex_male -----------------------------------------------
+
+if (active_analyses$sub_sex_male==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_sex=="Male",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_sex")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_sex_male.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_sex_male.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_age_18_39 ----------------------------------------------
+
+if (active_analyses$sub_age_18_39==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_num_age>=18 &
+                input$cov_num_age<40,]
+  
+  df[,c("sub_bin_covid19_confirmed_history")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_age_18_39.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_age_18_39.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_age_40_59 ----------------------------------------------
+
+if (active_analyses$sub_age_40_59==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_num_age>=40 &
+                input$cov_num_age<60,]
+  
+  df[,c("sub_bin_covid19_confirmed_history")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_age_40_59.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_age_40_59.rds"))
+  rm(df)
+  
+}
+# Make model input: sub_age_60_79 ----------------------------------------------
+
+if (active_analyses$sub_age_60_79==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_num_age>=60 &
+                input$cov_num_age<80,]
+  
+  df[,c("sub_bin_covid19_confirmed_history")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_age_60_79.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_age_60_79.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_age_80_110 ---------------------------------------------
+
+if (active_analyses$sub_age_80_110==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_num_age>=80 &
+                input$cov_num_age<111,]
+  
+  df[,c("sub_bin_covid19_confirmed_history")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_age_80_110.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_age_80_110.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_ethnicity_white ----------------------------------------
+
+if (active_analyses$sub_ethnicity_white==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_ethnicity=="White",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_ethnicity")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_ethnicity_white.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_ethnicity_white.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_ethnicity_black ----------------------------------------
+
+if (active_analyses$sub_ethnicity_black==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_ethnicity=="Black",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_ethnicity")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_ethnicity_black.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_ethnicity_black.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_ethnicity_mixed ----------------------------------------
+
+if (active_analyses$sub_ethnicity_mixed==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_ethnicity=="Mixed",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_ethnicity")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_ethnicity_mixed.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_ethnicity_mixed.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_ethnicity_asian ----------------------------------------
+
+if (active_analyses$sub_ethnicity_asian==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_ethnicity=="South Asian",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_ethnicity")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_ethnicity_asian.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_ethnicity_asian.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_ethnicity_other ----------------------------------------
+
+if (active_analyses$sub_ethnicity_other==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_ethnicity=="Other",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_ethnicity")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_ethnicity_other.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_ethnicity_other.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_priorhistory_true --------------------------------------
+
+if (active_analyses$priorhistory_var!="" & active_analyses$sub_priorhistory_true==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE,]
+  df <- dplyr::rename(df, "priorhistory" = active_analyses$priorhistory_var)
+  df <- df[df$priorhistory==TRUE,]
+  
+  df[,c("sub_bin_covid19_confirmed_history","priorhistory")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_priorhistory_true.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_priorhistory_true.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_priorhistory_false -------------------------------------
+
+if (active_analyses$priorhistory_var!="" & active_analyses$sub_priorhistory_false==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE,]
+  df <- dplyr::rename(df, "priorhistory" = active_analyses$priorhistory_var)
+  df <- df[df$priorhistory==FALSE,]
+  
+  df[,c("sub_bin_covid19_confirmed_history","priorhistory")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_priorhistory_false.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_priorhistory_false.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_smokingstatus_ever -------------------------------------
+
+if (active_analyses$sub_smokingstatus_ever==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_smoking_status=="Ever smoker",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_smoking_status")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_smokingstatus_ever.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_smokingstatus_ever.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_smokingstatus_never ------------------------------------
+
+if (active_analyses$sub_smokingstatus_never==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_smoking_status=="Never smoker",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_smoking_status")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_smokingstatus_never.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_smokingstatus_never.rds"))
+  rm(df)
+  
+}
+
+# Make model input: sub_smokingstatus_current ----------------------------------
+
+if (active_analyses$sub_smokingstatus_current==TRUE) {
+  
+  df <- input[input$sub_bin_covid19_confirmed_history==TRUE & 
+                input$cov_cat_smoking_status=="Current smoker",]
+  
+  df[,c("sub_bin_covid19_confirmed_history","cov_cat_smoking_status")] <- NULL
+  saveRDS(df, file = paste0("output/model_input/",active_analyses$name,"-sub_smokingstatus_current.rds"))
+  print(paste0("Saved: output/model_input/",active_analyses$name,"-sub_smokingstatus_current.rds"))
+  rm(df)
+  
+}
+
