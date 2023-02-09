f96d6053b86cd1bd896a2a8fd889a2c55456680f
A-Whitmarsh <alex.whitmarsh@bristol.ac.uk>
2023-02-09 18:54:09 +0000

    Stage 2

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -258,6 +258,18 @@ actions_list <- splice(
     highly_sensitive = list(
       cohort = glue("output/input_*.rds")
     )
+  ),
+
+  # #comment("Stage 2 - Missing - Table 1"),
+  action(
+    name = "stage2_missing_table1_all",
+    run = "r:latest analysis/descriptives/Stage2_missing_table1.R all",
+    needs = list("stage1_data_cleaning_all"),
+    moderately_sensitive = list(
+      Missing_RangeChecks = glue("output/not-for-review/Check_missing_range_*.csv"),
+      DateChecks = glue("output/not-for-review/Check_dates_range_*.csv"),
+      Descriptive_Table = glue("output/review/descriptives/Table1_*.csv")
+    )
   )
 )
 
