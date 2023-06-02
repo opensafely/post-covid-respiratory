79496db5f581fe693f55e8fc473c98b36c3fcee6
Venexia Walker <venexia.walker@bristol.ac.uk>
2023-06-02 15:24:10 +0100

    Update summarise_events.R

--- analysis/summarise_events.R
+++ analysis/summarise_events.R
@@ -1,18 +1,16 @@
-library(data.table)
-library(dplyr)
+# Code by Alex Whitmarsh
+
 library(Hmisc)
 library(readr)
-
-output_dir <- "C:/Users/rs22981/GitHub/post-covid-respiratory/output/"
 
 outcomes <- c("out_date_breathless", "out_date_cough", "out_date_urti", "out_date_asthma_exac", "out_date_copd_exac")
 
 #read datasets
-prevax <- read_csv("output/input_prevax.csv.gz") %>%
+prevax <- readr::read_csv("output/input_prevax.csv.gz") %>%
   select_if(names(.) %in% outcomes)
-vax <- read_csv("output/input_vax.csv.gz") %>%
+vax <- readr::read_csv("output/input_vax.csv.gz") %>%
   select_if(names(.) %in% outcomes)
-unvax <- read_csv("output/input_unvax.csv.gz") %>%
+unvax <- readr::read_csv("output/input_unvax.csv.gz") %>%
   select_if(names(.) %in% outcomes)
 
 #summary data 
