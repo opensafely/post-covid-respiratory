ef5bcfefd0b3b030dfd10f00571efa2e91006480
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-06-30 11:34:32 +0100

    summarise recurrent event counts

--- analysis/summarise_events.R
+++ analysis/summarise_events.R
@@ -73,7 +73,54 @@ for (cohort in c("prevax", "unvax", "vax")) {
   
   results <- tidyr::pivot_wider(results, names_from = "statistic", values_from = "value")
 
-# Save results -----------------------------------------------------------------
-print('Save results')
+  # Save results -----------------------------------------------------------------
+  print('Save results')
+  
+  write.csv(results, paste0("output/summarise_events_percentiles.csv"), row.names = FALSE)  
+  
+# alternative summary  -----------------------------------------------------------------  
+library(tidyverse)  
 
-write.csv(results, paste0("output/summarise_events.csv"), row.names = FALSE)
+# read the data and create cohort variable
+df <- map_df(
+  c("prevax", "unvax", "vax"),
+  ~readr::read_csv(file = paste0("output/input_",.x,".csv.gz")) %>% 
+    mutate(cohort = .x)
+)
+
+# bin the number of results into categories and count the number of patients 
+# in each category (within cohort and outcome)
+results2 <- df %>%
+  select(cohort, starts_with("out_date_")) %>%
+  mutate(
+    across(
+      starts_with("out_date_"),
+      ~cut(.x, breaks = c(0,1,2,3,5,10,20,50,100,Inf), right = FALSE)
+    )
+  ) %>%
+  pivot_longer(
+    cols = -cohort
+  ) %>%
+  group_by(cohort, name, value) %>%
+  count() %>%
+  ungroup() %>%
+  pivot_wider(names_from = value, values_from = n) %>%
+  # replace NAs with zeros
+  mutate(across(where(is.numeric), ~ replace_na(.x, 0L))) 
+           
+  roundmid_any <- function(x, to=1){
+    # like ceiling_any, but centers on (integer) midpoint of the rounding points
+    ceiling(x/to)*to - (floor(to/2)*(x!=0))
+  }
+  
+  # save unrounded results
+  results2 %>%
+    mutate(total = rowSums(across(where(is.numeric)))) %>%
+    write.csv("output/summarise_events_counts.csv", row.names = FALSE)  
+  
+  # save midpoint6 results
+  results2 %>%
+    mutate(across(where(is.numeric), ~roundmid_any(.x, to = 6))) %>%
+    mutate(total = rowSums(across(where(is.numeric)))) %>%
+    write.csv("output/summarise_events_counts_midpoint6.csv", row.names = FALSE)  
+  
