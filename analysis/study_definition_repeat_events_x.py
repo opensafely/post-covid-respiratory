5bf21511c7bb81102645b568c8e38cef479340c4
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-20 14:07:59 +0100

    Update study_definition_repeat_events_x.py

--- analysis/study_definition_repeat_events_x.py
+++ analysis/study_definition_repeat_events_x.py
@@ -12,6 +12,7 @@ from cohortextractor import (
   codelist,
   filter_codes_by_category,
   combine_codelists,
+  params
 )
 
 ## Codelists from codelist.py (which pulls them from the codelist folder)
@@ -23,33 +24,46 @@ import json
 # import clinical_event_date_X function
 from common_variables import clinical_event_date_X
 
-# max_n for each cohort and outcome
-with open("output/preprocess/max_events.json") as f:
+# max_n for each outcome
+with open("output/repeat_events/max_events.json") as f:
    max_events = json.load(f)
 
 #study_dates
 with open("output/study_dates.json") as f:
   study_dates = json.load(f)
 
+# params
+n_min=int(params["n_min"])
+n_max=int(params["n_max"])
+index_event = n_min - 1
+
 # extract repeat events
-def out_date_n(name, max_events=max_events):
+def out_date_n(
+      name, 
+      index_event=index_event,
+      n_min=n_min, 
+      n_max=n_max, 
+      max_events=max_events
+      ):
+    # redefine n_max to be the maximum of n_max and max_events["name"]
+    n_max = min(n_max, int(max_events[name]))
     # function for creating the out_date_5 variable
-    def out_date_5(name):
+    def out_date_index(name, index_event):
         return {
-            f"out_date_{name}_5": patients.with_value_from_file(
-                f_path=f"output/preprocess/out_date_5.csv.gz",
-                returning=f"out_date_{name}_5", 
+            f"out_date_{name}_{index_event}": patients.with_value_from_file(
+                f_path=f"output/repeat_events/out_date_{index_event}.csv.gz",
+                returning=f"out_date_{name}_{index_event}", 
                 returning_type="date", 
                 date_format='YYYY-MM-DD',
             )
         }
-    variables=out_date_5(name)
+    variables=out_date_index(name, index_event)
     variables.update(
        clinical_event_date_X(
             name=name,
-            index_date=f"out_date_{name}_5",
-            n=int(max_events[name]),
-            index_from=6,
+            index_date=f"out_date_{name}_{index_event}",
+            n=n_max,
+            index_from=n_min,
        )
     )
     return variables
@@ -65,7 +79,7 @@ study = StudyDefinition(
     },
 
     population=patients.which_exist_in_file(
-        f_path=f"output/preprocess/out_date_5.csv.gz"
+        f_path=f"output/repeat_events/out_date_{index_event}.csv.gz"
         ), 
 
     **out_date_n(name="breathless"),
