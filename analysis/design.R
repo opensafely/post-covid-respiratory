b9da116a4f32337975b01d65ded02008c02829ec
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-20 19:54:06 +0100

    Update design.R

--- analysis/design.R
+++ analysis/design.R
@@ -1,8 +1,15 @@
- 
-# the upper limit of the last increment (1000 here) just has to be some number
+library(tidyverse) 
+
+# repeat_events_increments can be adapted based on the results of the action `prelight_repeat_events_2`
+# The upper limit of the last increment (1000 here) just has to be some number
 # that must be larger than the maximum events that we are expecting across all 
 # outcomes.
-repeat_events_increments <- data.frame(min = c(1, 6, 21), max = c(5, 20, 1000))
+repeat_events_increments <- data.frame(max = c(5, 20, 1000)) %>%
+  mutate(
+    step = row_number(), 
+    min = lag(max, default = 0)+1,
+    .before = 1
+  )
 readr::write_csv(
   repeat_events_increments,
   here::here("lib", "repeat_events_increments.csv")
