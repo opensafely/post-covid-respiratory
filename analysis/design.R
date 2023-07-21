5d4622ca5333691a4dfa73c36a45032ff618f6a2
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-21 10:05:35 +0100

    Update design.R

--- analysis/design.R
+++ analysis/design.R
@@ -1,13 +1,24 @@
 library(tidyverse) 
 
-# repeat_events_increments can be adapted based on the results of the action `prelight_repeat_events_2`
-# The upper limit of the last increment (1000 here) just has to be some number
-# that must be larger than the maximum events that we are expecting across all 
-# outcomes.
-repeat_events_increments <- data.frame(max = c(5, 20, 1000)) %>%
+# `repeat_events_increments` specifies the lower and upper limits for the number
+# of events extracted in each study definition. E.g.:
+# 
+# - For study_definition_repeat_events_1.py, events `repeat_events_increments$lower[1]` 
+#   to `repeat_events_increments$upper[1]` are extracted.
+# 
+# - For study_definition_repeat_events_x.py with argument step, events 
+#   `repeat_events_increments$lower[step]` to `repeat_events_increments$upper[step]` 
+#   are extracted.
+# 
+# The upper limit of the final step (1000 here) must be larger than the maximum 
+# events that we are expecting across all outcomes. Inspect the log file for 
+# preflight_repeat_events_2 to make sure the values of `repeat_events_increments$upper`
+# are appropriate. Redefine if not and rerun preflight_repeat_events_2.
+# 
+repeat_events_increments <- data.frame(upper = c(5, 20, 1000)) %>%
   mutate(
     step = row_number(), 
-    min = lag(max, default = 0)+1,
+    lower = lag(upper, default = 0)+1,
     .before = 1
   )
 readr::write_csv(
