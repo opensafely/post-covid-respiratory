64ba7df4e8dac96a7b03818d4078fee224c175c9
ZoeMZou <mz16609@bristol.ac.uk>
2024-12-12 17:52:16 +0000

    Update preprocess_data.R

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -32,8 +32,7 @@ print(all_cols)
 
 cat_cols <- c("patient_id", grep("_cat", all_cols, value = TRUE))
 
-bin_cols <- c(grep("_bin", all_cols, value = TRUE),
-              "has_follow_up_previous_6months", "was_alive", "has_died")
+bin_cols <- c(grep("_bin", all_cols, value = TRUE))
 
 num_cols <- c(grep("_num", all_cols, value = TRUE),
               grep("vax_jcvi_age_", all_cols, value = TRUE))
@@ -66,9 +65,7 @@ message(paste0("Dataset has been read successfully with N = ", nrow(df), " rows"
 
 df <- df %>%
   mutate(across(all_of(date_cols),
-                ~ floor_date(as.Date(., format="%Y-%m-%d"), unit = "days")),
-         across(contains('_birth_year'),
-                ~ format(as.Date(., origin = "1970-01-01"), "%Y")))
+                ~ floor_date(as.Date(., format="%Y-%m-%d"), unit = "days")))
 
 # Overwrite vaccination information for dummy data and vax cohort only ---------
 
