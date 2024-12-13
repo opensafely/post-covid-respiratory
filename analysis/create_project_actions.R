74082abc2df2f440e955c72578e911a0044eaa6e
ZoeMZou <mz16609@bristol.ac.uk>
2024-12-13 12:06:24 +0000

    Update project actions

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -88,7 +88,7 @@ preprocess_data <- function(cohort){
       name = glue("preprocess_data_{cohort}"),
       run = glue("r:latest analysis/preprocess/preprocess_data.R"),
       arguments = c(cohort),
-      needs = list("generate_index_dates",glue("generate_study_population_{cohort}")),
+      needs = list("generate_dataset_index_dates",glue("generate_study_population_{cohort}")),
       moderately_sensitive = list(
         describe = glue("output/not-for-review/describe_input_{cohort}_stage0.txt"),
         describe_venn = glue("output/not-for-review/describe_venn_{cohort}.txt")
