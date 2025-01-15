c1120fce626fbab2de527a66e5e0fb1193fd8f58
ZoeMZou <mz16609@bristol.ac.uk>
2025-01-15 16:40:42 +0000

    Update preprocess_data.R

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -67,8 +67,10 @@ df <- df %>%
   mutate(across(all_of(date_cols),
                 ~ floor_date(as.Date(., format="%Y-%m-%d"), unit = "days")),
          across(contains('_birth_year'), 
-                ~ format(as.Date(., origin = "1970-01-01"), "%Y")))
-
+                ~ format(as.Date(., origin = "1970-01-01"), "%Y")),
+         across(all_of(num_cols), ~ as.numeric(.)), 
+         across(all_of(cat_cols), ~ as.factor(.))) 
+         
 # Overwrite vaccination information for dummy data and vax cohort only ---------
 
 if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations") &&
