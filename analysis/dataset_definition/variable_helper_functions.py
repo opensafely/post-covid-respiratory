e2cd949ca79fce772847842a23230fa431fa7062
ZoeMZou <mz16609@bristol.ac.uk>
2025-01-28 18:31:36 +0000

    Update variable_helper_functions.py

--- analysis/dataset_definition/variable_helper_functions.py
+++ analysis/dataset_definition/variable_helper_functions.py
@@ -154,3 +154,7 @@ def matching_death_between(codelist, start_date, end_date, where=True):
     return(
         ons_deaths.cause_of_death_is_in(codelist) & ons_deaths.date.is_on_or_between(start_date, end_date)
     )
+
+# filter a codelist based on whether its values included a specified set of allowed values (include)
+def filter_codes_by_category(codelist, include):
+    return {k:v for k,v in codelist.items() if v in include}
