fe5f71788da7aa3d6ebc7adc4e81b2691e0beb6a
ZoeMZou <mz16609@bristol.ac.uk>
2025-01-28 18:00:32 +0000

    update data_cleaning using R functions

--- analysis/data_cleaning/data_cleaning.R
+++ analysis/data_cleaning/data_cleaning.R
@@ -22,6 +22,7 @@ print('Source common functions')
 source("analysis/utility.R")
 source("analysis/data_cleaning/fn-qa.R")
 source("analysis/data_cleaning/fn-inex.R")
+source("analysis/data_cleaning/fn-ref.R")
 # Specify command arguments ----------------------------------------------------
 print('Specify command arguments')
 
@@ -44,7 +45,6 @@ print('Specify relevant dates')
 vax_start_date <- as.Date(study_dates$vax1_earliest, format="%Y-%m-%d")
 mixed_vax_threshold <- as.Date("2021-05-07")
 start_date_delta <- as.Date(study_dates$delta_date, format="%Y-%m-%d")
-end_date_delta <- as.Date(study_dates$omicron_date, format="%Y-%m-%d") 
 
 # Load cohort data -------------------------------------------------------------
 print('Load cohort data')
@@ -52,114 +52,24 @@ print('Load cohort data')
 input <- read_rds(file.path("output", paste0("input_",cohort,".rds")))
 print(paste0(cohort,  " cohort: ", nrow(input), " rows in the input file"))
 
-# Handle missing values --------------------------------------------------------
-print('Handle missing values')
+# Set reference levels for factors----------------------------------------------
 
-input$cov_cat_smoking_status <- replace(input$cov_cat_smoking_status, 
-                                        is.na(input$cov_cat_smoking_status),
-                                        "M")
+print('Call reference function')
 
-input <- input %>% 
-  mutate(cov_cat_region = as.character(cov_cat_region)) %>%
-  mutate(cov_cat_region = replace_na(cov_cat_region, "Missing")) %>%
-  mutate(cov_cat_region = as.factor(cov_cat_region))
-
-# Set reference levels for factors ---------------------------------------------
-print('Set reference levels for factors')
-
-cat_factors <- colnames(input)[grepl("_cat_",colnames(input))]
-input[,cat_factors] <- lapply(input[,cat_factors], function(x) factor(x, ordered = FALSE))
-
-# Set reference level for variable: sub_cat_covid19_hospital -------------------
-print('Set reference level for variable: sub_cat_covid19_hospital')
-
-input$sub_cat_covid19_hospital <- ordered(input$sub_cat_covid19_hospital, 
-                                          levels = c("non_hospitalised",
-                                                     "hospitalised",
-                                                     "no_infection"))
-
-# Set reference level for variable: cov_cat_ethnicity --------------------------
-print('Set reference level for variable: cov_cat_ethnicity')
-
-levels(input$cov_cat_ethnicity) <- list("Missing" = "0", "White" = "1", 
-                                        "Mixed" = "2", "South Asian" = "3", 
-                                        "Black" = "4", "Other" = "5")
-
-input$cov_cat_ethnicity <- ordered(input$cov_cat_ethnicity, 
-                                   levels = c("White","Mixed",
-                                              "South Asian","Black",
-                                              "Other","Missing"))
-
-# Set reference level for variable: cov_cat_imd -------------------------------
-print('Set reference level for variable: cov_cat_imd')
-
-input$cov_cat_imd <- ordered(input$cov_cat_imd, 
-                                     levels = c("1 (most deprived)","2","3","4","5 (least deprived)"))
-
-# Set reference level for variable: cov_cat_region -----------------------------
-print('Set reference level for variable: cov_cat_region')
-
-input$cov_cat_region <- relevel(input$cov_cat_region, ref = "East")
-
-# Set reference level for variable: cov_cat_smoking_status ---------------------
-print('Set reference level for variable: cov_cat_smoking_status')
-
-levels(input$cov_cat_smoking_status) <- list("Ever smoker" = "E", "Missing" = "M", "Never smoker" = "N", "Current smoker" = "S")
-
-input$cov_cat_smoking_status <- ordered(input$cov_cat_smoking_status, levels = c("Never smoker","Ever smoker","Current smoker","Missing"))
-
-# Set reference level for variable: cov_cat_sex --------------------------------
-print('Set reference level for variable: cov_cat_sex')
-
-levels(input$cov_cat_sex) <- list("Female" = "female", "Male" = "male", "Unknown" = "unknown")
-
-# # Replace NA values with "Unknown" to ensure patients with missing sex 
-# # are not evaluated entirely to NA during quality assurance steps
-
-input$cov_cat_sex <- replace(input$cov_cat_sex, is.na(input$cov_cat_sex), "Unknown")
-
-
-input$cov_cat_sex <- factor(input$cov_cat_sex, 
-                            levels = c("Female", "Male", "Unknown"))
-input$cov_cat_sex <- relevel(input$cov_cat_sex, ref = "Female")
-
-
-# Set reference level for variable: vax_cat_jcvi_group -------------------------
-print('Set reference level for variable: vax_cat_jcvi_group')
-
-input$vax_cat_jcvi_group <- ordered(input$vax_cat_jcvi_group, 
-                                    levels = c("12","11","10",
-                                               "09","08","07",
-                                               "06","05","04",
-                                               "03","02","01","99"))
-
-# Set reference level for binaries ---------------------------------------------
-print('Set reference level for binaries')
-
-bin_factors <- colnames(input)[grepl("cov_bin_",colnames(input))]
-
-input[,bin_factors] <- lapply(input[,bin_factors], 
-                              function(x) factor(x, levels = c("FALSE","TRUE")))
-
-# Specify consort table --------------------------------------------------------
-print('Specify consort table')
-
-consort <- data.frame(Description = "Input",
-                      N = nrow(input),
-                      stringsAsFactors = FALSE)
+input <- ref(input)$input
 
 # Quality assurance ------------------------------------------------------------
 
 print('Call quality assurance function')
-qa_results <- qa(input, study_dates, consort, cohort, threshold)
+qa_results <- qa(input, study_dates)
 input <- qa_results$input
 consort <- qa_results$consort
 
 # Inclusion criteria -----------------------------------------------------------
 
 print('Call inclusion criteria function')
-input <- inex(input, consort, cohort, threshold)$input
-consort <- inex(input, consort, cohort, threshold)$consort
+input <- inex(input, consort, cohort, vax_start_date, mixed_vax_threshold, start_date_delta)$input
+consort <- inex(input, consort, cohort, vax_start_date, mixed_vax_threshold, start_date_delta)$consort
 
 # Save consort data after Inclusion criteria
 print('Saving consort data after Inclusion criteria')
@@ -185,7 +95,7 @@ write.csv(consort, file = paste0("output/consort_", cohort, "_midpoint6.csv"), r
 # Save the dataset 
 print('Saving dataset after Inclusion criteria')
 
-input <- input[, c("patient_id", "cens_date_death", "index_date",
+input <- input[, c("patient_id", "index_date",
                   colnames(input)[grepl("end_date_", colnames(input))],
                   colnames(input)[grepl("sub_", colnames(input))],
                   colnames(input)[grepl("exp_", colnames(input))],
