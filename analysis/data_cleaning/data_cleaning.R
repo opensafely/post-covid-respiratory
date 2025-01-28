7cb1396788335f54418fe56381e34d4d0066ea9c
ZoeMZou <mz16609@bristol.ac.uk>
2025-01-28 15:46:49 +0000

    update data_cleaning

--- analysis/data_cleaning/data_cleaning.R
+++ analysis/data_cleaning/data_cleaning.R
@@ -158,4 +158,41 @@ consort <- qa_results$consort
 # Inclusion criteria -----------------------------------------------------------
 
 print('Call inclusion criteria function')
-inex(input, consort, cohort, threshold)
+input <- inex(input, consort, cohort, threshold)$input
+consort <- inex(input, consort, cohort, threshold)$consort
+
+# Save consort data after Inclusion criteria
+print('Saving consort data after Inclusion criteria')
+
+consort$N <- as.numeric(consort$N)
+consort$removed <- dplyr::lag(consort$N, default = dplyr::first(consort$N)) - consort$N
+
+write.csv(consort, file = paste0("output/consort_", cohort, ".csv"), row.names = FALSE)
+
+# Perform redaction
+print('Performing redaction')
+
+consort$removed <- NULL
+consort$N_midpoint6 <- roundmid_any(consort$N, to=threshold)
+consort$removed_derived <- dplyr::lag(consort$N_midpoint6, default = dplyr::first(consort$N_midpoint6)) - consort$N_midpoint6
+consort$N <- NULL
+
+# Save rounded consort data
+print('Saving rounded consort data after Inclusion criteria')
+
+write.csv(consort, file = paste0("output/consort_", cohort, "_midpoint6.csv"), row.names = FALSE)
+
+# Save the dataset 
+print('Saving dataset after Inclusion criteria')
+
+input <- input[, c("patient_id", "cens_date_death", "index_date",
+                  colnames(input)[grepl("end_date_", colnames(input))],
+                  colnames(input)[grepl("sub_", colnames(input))],
+                  colnames(input)[grepl("exp_", colnames(input))],
+                  colnames(input)[grepl("out_", colnames(input))],
+                  colnames(input)[grepl("cov_", colnames(input))],
+                  colnames(input)[grepl("cens_", colnames(input))],
+                  colnames(input)[grepl("vax_date_", colnames(input))],
+                  colnames(input)[grepl("vax_cat_", colnames(input))])]
+
+saveRDS(input, file = paste0("output/input_", cohort, ".rds"), compress = TRUE)
