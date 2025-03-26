ed5dd9ad7838216a1e7171d669a0103515d2e3b0
ZoeMZou <mz16609@bristol.ac.uk>
2025-03-26 16:07:40 +0000

    Update fn-qa.R

--- analysis/dataset_clean/fn-qa.R
+++ analysis/dataset_clean/fn-qa.R
@@ -1,51 +1,85 @@
 # Function to apply quality assurance
 qa <- function(input, flow, study_dates) {
-  print('Quality assurance: Year of birth is missing')
+  print(
+    'Quality assurance: Year of birth is before year of death (if year of death is available)'
+  )
 
-  input <- input[!is.na(input$qa_num_birth_year), ]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Year of birth is missing",
-                                 nrow(input))
+  input <- input[
+    ((!is.na(input$cens_date_death)) &
+      (year(input$cens_date_death) >= input$qa_num_birth_year)) |
+      (is.na(input$cens_date_death)),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Year of birth is before year of death (if year of death is available)",
+    nrow(input)
+  )
 
-  print('Quality assurance: Year of birth is after year of death')
+  print(
+    'Quality assurance: Year of birth is before today (implemented using last data collection date)'
+  )
 
-  input <- input[((!is.na(input$cens_date_death)) & 
-                  (year(input$cens_date_death) >= input$qa_num_birth_year)) |
-                 (is.na(input$cens_date_death)), ]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Year of birth is after year of death",
-                                 nrow(input))
-  
-  print('Quality assurance: Year of birth exceeds current date')
-  
-  input <- input[(input$qa_num_birth_year <= format(Sys.Date(), "%Y")), ]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Year of birth exceeds current date",
-                                 nrow(input))
-  
-  print('Quality assurance: Date of death is invalid (after current date)')
-  
-  input <- input[(!is.na(input$cens_date_death) &  
-  (input$cens_date_death <= format(Sys.Date(),"%Y-%m-%d"))) | 
-  is.na(input$cens_date_death),]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Date of death is invalid (after current date)",
-                                 nrow(input))
+  input <- input[
+    !is.na(input$qa_num_birth_year) &
+      (input$qa_num_birth_year <= format(Sys.Date(), "%Y")),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Year of birth is before today (implemented using last data collection date)",
+    nrow(input)
+  )
 
-  print('Quality assurance: Pregnancy/birth codes for men')
-  
-  input <- input[!(input$qa_bin_pregnancy == TRUE & input$cov_cat_sex=="male") | is.na(input$cov_cat_sex),]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Pregnancy/birth codes for men",
-                                 nrow(input))
-  
-  print('Quality assurance: HRT or COCP meds for men')
-  
-  input <- input[!(input$cov_cat_sex=="male" & input$qa_bin_hrtcocp==TRUE) | is.na(input$cov_cat_sex),]
-  flow[nrow(flow)+1,] <- c("Quality assurance: HRT or COCP meds for men",
-                                 nrow(input))
-  
-  print('Quality assurance: Prostate cancer codes for women')
-  
-  input <- input[!(input$qa_bin_prostate_cancer == TRUE & 
-                     input$cov_cat_sex=="female") | is.na(input$cov_cat_sex),]
-  flow[nrow(flow)+1,] <- c("Quality assurance: Prostate cancer codes for women",
-                                 nrow(input))
+  print(
+    'Quality assurance: Date of death is before today (if year of death is available and implemented using last data collection date)'
+  )
+
+  input <- input[
+    (!is.na(input$cens_date_death) &
+      (input$cens_date_death <= format(Sys.Date(), "%Y-%m-%d"))) |
+      is.na(input$cens_date_death),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Date of death is before today (if year of death is available and implemented using last data collection date)",
+    nrow(input)
+  )
+
+  print(
+    'Quality assurance: Men do not have records that contain pregnancy and/or birth codes'
+  )
+
+  input <- input[
+    !(input$qa_bin_pregnancy == TRUE & input$cov_cat_sex == "male") |
+      is.na(input$cov_cat_sex),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Men do not have records that contain pregnancy and/or birth codes",
+    nrow(input)
+  )
+
+  print(
+    'Quality assurance: Men do not have records that contain HRT or COCP medication codes'
+  )
+
+  input <- input[
+    !(input$cov_cat_sex == "male" & input$qa_bin_hrtcocp == TRUE) |
+      is.na(input$cov_cat_sex),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Men do not have records that contain HRT or COCP medication codes",
+    nrow(input)
+  )
+
+  print(
+    'Quality assurance: Women do not have records that contain prostate cancer codes'
+  )
+
+  input <- input[
+    !(input$qa_bin_prostate_cancer == TRUE &
+      input$cov_cat_sex == "female") |
+      is.na(input$cov_cat_sex),
+  ]
+  flow[nrow(flow) + 1, ] <- c(
+    "Quality assurance: Women do not have records that contain prostate cancer codes",
+    nrow(input)
+  )
 
   return(list(input = input, flow = flow))
 }
