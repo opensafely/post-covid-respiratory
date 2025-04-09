8f660980b72f6f45789d87051d839fa481bcf6eb
Harry T Mason <harry.mason@outlook.com>
2025-04-09 19:54:37 +0100

    Making sure str_to_title is loaded as a function

--- analysis/model/make_model_input.R
+++ analysis/model/make_model_input.R
@@ -3,6 +3,7 @@ print("Load packages")
 
 library(magrittr)
 library(data.table)
+library(stringr)
 
 # Source functions -------------------------------------------------------------
 print("Source functions")
@@ -23,7 +24,7 @@ if (length(args) == 0) {
   # name <- "cohort_vax-subhistory_migraine; cohort_vax-subhistory_depression" # This one should fail (it's a neuro group)
   # name <- "cohort_vax-sub_age_18_39_preex_FALSE-pf;cohort_vax-sub_age_40_59_preex_FALSE-pf;cohort_vax-sub_age_60_79_preex_FALSE-pf;cohort_vax-sub_age_80_110_preex_FALSE-pf" # This is a test for 4 at once
   # name <- "cohort_prevax-sub_ethnicity_white_preex_FALSE-copd;cohort_prevax-sub_ethnicity_black_preex_FALSE-copd;cohort_prevax-sub_ethnicity_mixed_preex_FALSE-copd;cohort_prevax-sub_ethnicity_south_asian_preex_FALSE-copd;cohort_prevax-sub_ethnicity_other_preex_FALSE-copd"
-  # name <- "cohort_prevax-sub_ethnicity_south_asian_preex_FALSE-copd;" # Check that the "south_asian" string is being processed correctly
+  name <- "cohort_prevax-sub_ethnicity_south_asian_preex_FALSE-copd;" # Check that the "south_asian" string is being processed correctly
   # name <- "cohort_vax-sub_covidhistory_preex_FALSE-pf;cohort_vax-sub_covidhistory_preex_TRUE-pf" # chacking pre-ex and that covidhistory is behaving as expected
 } else {
   name <- args[[1]]
