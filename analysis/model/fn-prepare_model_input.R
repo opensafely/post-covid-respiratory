9fe3d88d8bd9469f7fbee94de3a0cc392fbae761
Venexia Walker <venexia.walker@bristol.ac.uk>
2025-04-17 16:33:16 +0100

    Origin for as.Date()

--- analysis/model/fn-prepare_model_input.R
+++ analysis/model/fn-prepare_model_input.R
@@ -79,12 +79,12 @@ prepare_model_input <- function(name) {
         out_date > end_date_outcome | out_date < index_date,
         NA,
         out_date
-      )),
+      ), origin = "1970-01-01"),
       exp_date = as.Date(ifelse(
         exp_date > end_date_exposure | exp_date < index_date,
         NA,
         exp_date
-      )),
+      ), origin = "1970-01-01"),
       sub_cat_covidhospital = ifelse(
         is.na(exp_date),
         "no_infection",
